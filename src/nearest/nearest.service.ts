175d3b4d0c45f408fe74fb75bb04a55a3e5e3572
Ashutosh Rai <ashu00jnp@gmail.com>
2024-01-05 03:04:04 +0530

    redis geoadd

--- src/nearest/nearest.service.ts
+++ src/nearest/nearest.service.ts
@@ -2,10 +2,11 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateNearestDto } from './dto/create-nearest.dto';
 import { UpdateNearestDto } from './dto/update-nearest.dto';
 import Redis from "ioredis"
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NearestService {
-  @Inject('REDIS_CLIENT') private redis: Redis
+  constructor(private readonly configService:ConfigService,@Inject('REDIS_CLIENT') private client){}
 
 
 
@@ -14,7 +15,17 @@ export class NearestService {
   }
 
   async findAll() {
-    const get=this.redis.get('ping')
+    const client = new Redis(`rediss://default:${this.configService.get('REDIS_PASS')}@usw1-fresh-gnu-33499.upstash.io:33499`);
+    // const get=this.redis.geosearch('Ashutosh', 30.2343348, 30.243243, 50, "km", function(err, reply) {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    
+    //   console.log(reply);
+    // })
+    const get= await client.geodist('Ashutosh','aiushi quiboai we', 'Vijay vihar')
+    // const get=await client.geoadd('Dheeraj',21.23434, 35.233532, 'Vijay Vihar');
     return get;
   }
 
